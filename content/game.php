8511fce3265014a02969a93cd1d567d389bfd044
Jeo <j.cabrigas.writer@gmail.com>
2025-03-24 04:48:05 -0700

    Adds general text to the content pages.

---
+++ content/game.php
@@ -0,0 +1,2 @@
+<!-- Game Area -->
+<h1>This is where the game will be placed for players to interact with.</h1>
---
+++ content/rules.php
@@ -0,0 +1,2 @@
+<!-- Rules Area -->
+ <h1>This is where players will go if they need an explanation on how the game works.</h1>
