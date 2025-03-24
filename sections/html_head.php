959683c3c0e4cfaf3a2add135c789a04b7b90b6b
Jeo <j.cabrigas.writer@gmail.com>
2025-03-24 04:41:11 -0700

    Renames file in sections folder to Nav. Moves general content from index.php to their respective files in the sections folder. Includes sections files in index.php.

---
+++ sections/html_head.php
@@ -0,0 +1,25 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<meta name="author" content="Just Jeo" />    
+<meta name="description" content="A game where players try to guess if their number is higher or lower than the computer's card.">
+
+<!-- Favicon icon -->
+<link rel="icon" type="image/png" href="assets/images/favicon.png">  
+
+<!-- Document Title -->
+    <title>Jeo's Higher Or Lower Game</title>
+
+<!-- File Connections -->
+<link rel="stylesheet" href="assets/css/main.css">
+
+<!-- Font Family -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Playpen+Sans" rel="stylesheet">
+
+<!-- Bootstrap Ver 5.3 -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<!-- AYO! This is Jeo! -->
