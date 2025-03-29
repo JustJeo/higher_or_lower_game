153a4db75d65be48d2dd42867e2362d8c0dd5906
Jeo <j.cabrigas.writer@gmail.com>
2025-03-29 15:58:23 -0700

    Changes syntax of footer section to match rest of project. Adds padding to footer.

--- sections/footer.php
+++ sections/footer.php
@@ -1,4 +1,4 @@
-<section id="version-number-and-slogan" class="text-center">
+<section id="version_number_and_slogan" class="text-center">
     <small>Ver. 0.25.03.28</small>
     <small>Be you; just you</small>
 </section>
