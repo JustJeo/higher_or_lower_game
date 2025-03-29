fb3e0b08fe28bf350a70c607fd347fc9c185381f
Jeo <j.cabrigas.writer@gmail.com>
2025-03-29 15:39:52 -0700

    Adds Start btn to landing page.

--- content/landing.php
+++ content/landing.php
@@ -3,4 +3,7 @@
     <div id="game_title_container">
         <h1 id="game_title">Higher or Lower?</h1>
     </div>
+    <div id="start_button_container">
+        <button type="button">Start Game</button>
+    </div>
 </section>
