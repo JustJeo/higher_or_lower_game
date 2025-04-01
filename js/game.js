9267b236bbd83080d126f24fd3640242088b05c3
Jeo <j.cabrigas.writer@gmail.com>
2025-04-01 13:12:50 -0700

    Adds logic to fetching js file. Creates new fx to game_logic.js

--- js/game.js
+++ js/game.js
@@ -1,19 +1,24 @@
 // Fetches separated .html files to inject into index.html
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("sections/html_head.html")
-        .then(response => response.text())
-        .then(data => document.getElementById("html_head").innerHTML = data);
+    Promise.all([
+        fetch("sections/html_head.html")
+            .then(response => response.text())
+            .then(data => document.getElementById("html_head").innerHTML = data),
 
-    fetch("sections/nav.html")
-        .then(response => response.text())
-        .then(data => document.getElementById("nav").innerHTML = data);
+        fetch("sections/nav.html")
+            .then(response => response.text())
+            .then(data => document.getElementById("nav").innerHTML = data),
 
-    fetch("content/game.html")
-        .then(response => response.text())
-        .then(data => document.getElementById("game_content").innerHTML = data);
+        fetch("content/game.html")
+            .then(response => response.text())
+            .then(data => document.getElementById("game_content").innerHTML = data),
 
-    fetch("sections/footer.html")
-        .then(response => response.text())
-        .then(data => document.getElementById("footer").innerHTML = data);
+        fetch("sections/footer.html")
+            .then(response => response.text())
+            .then(data => document.getElementById("footer").innerHTML = data)
+         ]).then(() => {
+            console.log("All Sections loaded.");
+            startGameLogic();
+    });
 });
 
--- js/game_logic.js
+++ js/game_logic.js
@@ -1,6 +1,10 @@
-// PLAYER CLICKS "START GAME" BTN
-const startGame = document.getElementById("start_game_btn")
-startGame.addEventListener("click", () => {
-    document.getElementById("start_game_btn").style.display = "none";
-    document.getElementById("game_play_container").style.display = "block";
-});
+function startGameLogic() {
+    console.log("Game Logic Starts")
+
+    // PLAYER CLICKS "START GAME" BTN
+    const startGame = document.getElementById("start_game_btn")
+    startGame.addEventListener("click", () => {
+        document.getElementById("start_game_btn").style.display = "none";
+        document.getElementById("game_play_container").style.display = "block";
+    });
+};
