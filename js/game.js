7614668c4aee8384fefdf4ffbf40e87c135dafe8
Jeo <j.cabrigas.writer@gmail.com>
2025-04-01 12:51:59 -0700

    Creates game_logic.js file to separate js for fetching and js for game logic. Moves game logic from game.js to game_logic.js. Adds connection of game_logic.js to index.html

--- js/game.js
+++ js/game.js
@@ -10,15 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     fetch("content/game.html")
         .then(response => response.text())
-        .then(data => {document.getElementById("game_content").innerHTML = data;
-            // PLAYER CLICKS "START GAME" BTN
-            const startGame = document.getElementById("start_game_btn")
-            startGame.addEventListener("click", () => {
-                document.getElementById("start_game_btn").style.display = "none";
-                document.getElementById("game_play_container").style.display = "block";
-            });
-        })
-        .catch(error => console.error('Error loading game.html:', error));
+        .then(data => document.getElementById("game_content").innerHTML = data);
 
     fetch("sections/footer.html")
         .then(response => response.text())
---
+++ js/game_logic.js
@@ -0,0 +1,6 @@
+// PLAYER CLICKS "START GAME" BTN
+const startGame = document.getElementById("start_game_btn")
+startGame.addEventListener("click", () => {
+    document.getElementById("start_game_btn").style.display = "none";
+    document.getElementById("game_play_container").style.display = "block";
+});
