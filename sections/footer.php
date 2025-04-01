a3296d93431e33982c9ad0fd7817f74dac7cb55c
Jeo <j.cabrigas.writer@gmail.com>
2025-03-31 23:48:15 -0700

    Converts initial .php files to .html files.

--- sections/footer.php
+++
@@ -1,6 +0,0 @@
-<footer id="footer">
-    <section id="version_number_and_slogan" class="text-center">
-        <small>Ver. 0.25.03.28</small>
-        <small>Be you; just you</small>
-    </section>
-</footer>
