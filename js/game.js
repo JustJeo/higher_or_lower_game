064e46ab1e48ecfda2dbf75cf8c7343b1582df8e
Jeo <j.cabrigas.writer@gmail.com>
2025-04-01 12:14:00 -0700

    Hides game play area on load. Adds logic to show game area when Start btn is clicked.

--- js/game.js
+++ js/game.js
@@ -15,6 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const startGame = document.getElementById("start_game_btn")
             startGame.addEventListener("click", () => {
                 document.getElementById("start_game_btn").style.display = "none";
+                document.getElementById("game_play_container").style.display = "block";
             });
         })
         .catch(error => console.error('Error loading game.html:', error));
